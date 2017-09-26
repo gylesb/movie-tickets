879073eb6e33800ed6cc31f9b9b0fe2f5ec896cf
Daniel Cheng and Gyles Batara <daniel@>
2017-09-26 13:45:55 -0700

    Update age function

--- js/scripts.js
+++ js/scripts.js
@@ -22,14 +22,14 @@ Movie.prototype.ticket = function(age) {
 // User Logic
 $(document).ready(function() {
   // creating an object using constructor
-  var kingsman = new Movie("Kingsman\: The Golden Circle", "PG-13", "yes", 15.00, 1);
+  var kingsman = new Movie("Kingsman: The Golden Circle", "PG-13", "yes", 15.00, 1);
   var it = new Movie("IT (2017)", "R", "yes", 15.00, 2);
   var hitch = new Movie("Hitch", "PG-13", "no", 10.00, 3);
   var transform = new Movie("Transformers: The Last Knight", "PG-13", "no", 10.00, 4);
   var mother = new Movie("Mother", "R", "yes", 15.00, 5);
   var american = new Movie("American Assassin", "R", "yes", 15.00, 6);
   var moana = new Movie("Moana", "PG", "no", 10.00, 7);
-  var samurai = new Movie("The Last Samurai", "PG-13", "no", 10, 8);
+  var samurai = new Movie("The Last Samurai", "R", "no", 10.00, 8);
 
   var movieArray = [kingsman, it, hitch, transform, mother, american, moana, samurai];
 
@@ -46,17 +46,25 @@ $(document).ready(function() {
     var movieDate = $("input#date").val();
     var movieTime = $("#time").val();
     var age = parseInt($("input#age").val());
-    var price = 0;
+    var price = 15;
     movieArray.forEach(function(movie) {
       if (movie.id === movieSelect) {
         console.log(movie.movieRating);
         console.log("YOUR MOVIE IS: " + movie.movieName);
-        price = movie.ticket();
+        price = movie.ticket(age);
+
+        // if (age < 17 && movie.movieRating == "R") {
+        //   alert("You aren't old enough to watch this movie");
+        // } else if (age > 65) {
+        //   price = movie.moviePrice - 5;
+        // } else
+        // price = movie.moviePrice;
+
         $("#resultBox").show();
         $("#price h4").append(movie.movieName);
         $("#price h2").append("$"+price+".00");
       }
-    })
+    });
     // alert(movieSelect);
     // myMovie = window[movieSelect];
 
